f1ffec666f8b3ccd49b761dab0d4f7261352c065
Dmitry Malakhov <byuarus@gmail.com>
2020-11-24 14:05:01 -0800

    Migration to SPM part 2

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftIO",
-            dependencies: ["SwiftIOSupport"]),
+            dependencies: ["SwiftIOSupport", "SwiftUtilities"]),
         .target(
             name: "SwiftIOSupport",
             dependencies: []
